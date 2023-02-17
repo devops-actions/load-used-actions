f630e272f9f4eb6922f76b8c25dbd022d7169ab1
Rob Bos <rajbos@users.noreply.github.com>
2023-02-17 11:30:26 +0100

    Update entrypoint.ps1 (#24)
    Write to the output file instead of set-output

--- Src/PowerShell/entrypoint.ps1
+++ Src/PowerShell/entrypoint.ps1
@@ -29,8 +29,7 @@ function main {
     Write-Host "Found [$($actions.Count)] actions "
     #Write-Verbose $actions | ConvertTo-Json -Depth 10
     $jsonObject = ($actions | ConvertTo-Json -Depth 10 -Compress)
-    
-    Write-Output "::set-output name=actions::'$jsonObject'"
+    echo "actions='$jsonObject'" >> $env:GITHUB_OUTPUT
     Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
 }
 
