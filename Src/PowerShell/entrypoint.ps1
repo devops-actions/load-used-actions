ea9e791da21a13089fafbe25a745a7f920ca622d
Rob Bos <raj.bos@gmail.com>
2023-04-02 19:00:27 +0200

    Validating

--- Src/PowerShell/entrypoint.ps1
+++ Src/PowerShell/entrypoint.ps1
@@ -29,8 +29,8 @@ function main {
     Write-Host "Found [$($actions.Count)] actions "
     #Write-Verbose $actions | ConvertTo-Json -Depth 10
     $jsonObject = ($actions | ConvertTo-Json -Depth 10 -Compress)
-    Write-Host "actions='$jsonObject'" >> $env:GITHUB_OUTPUT
-    Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
+    Set-Content -Value "actions='$jsonObject'" -Path $env:GITHUB_OUTPUT
+    Write-Host "Stored actions in outputs list $($env:GITHUB_OUTPUT). Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
 }
 
 try {
