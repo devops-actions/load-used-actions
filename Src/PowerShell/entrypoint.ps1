47567b58253895662a489000af7ee118c06d0f63
Rob Bos <raj.bos@gmail.com>
2023-04-02 18:55:20 +0200

    Convert echo into pwsh

--- Src/PowerShell/entrypoint.ps1
+++ Src/PowerShell/entrypoint.ps1
@@ -29,7 +29,7 @@ function main {
     Write-Host "Found [$($actions.Count)] actions "
     #Write-Verbose $actions | ConvertTo-Json -Depth 10
     $jsonObject = ($actions | ConvertTo-Json -Depth 10 -Compress)
-    echo "actions='$jsonObject'" >> $env:GITHUB_OUTPUT
+    Write-Host "actions='$jsonObject'" >> $env:GITHUB_OUTPUT
     Write-Host "Stored actions in outputs list. Use $${{ steps.<step id>.outputs.actions }} in next action to load the json"
 }
 
