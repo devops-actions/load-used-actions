70f68835844f600aeaf90ce4bbace85394d239f2
Rob Bos <rajbos@users.noreply.github.com>
2023-09-04 13:01:19 +0000

    Fix issue loading pwsh mod in Codespace

--- Src/PowerShell/generic.ps1
+++ Src/PowerShell/generic.ps1
@@ -1,7 +1,18 @@
+$moduleName = "powershell-yaml"
 
 # install a yaml parsing module (already done in the container image)
 if($env:computername -ne "ROB-XPS9700") {
     Write-Host "PSHOME: [$pshome]" 
+
+    # check if module is installed locally:
+    $module = Get-Module -name $moduleName
+    if ($null -eq $module) {
+        Write-Host "Module [$moduleName] not found, installing it"
+        Install-Module -Name $moduleName -Force -Scope CurrentUser -AllowClobber
+    }
+    else {
+        Write-Host "Module [$moduleName] found, skipping installation"
+    }
 
     # add back the root folder to the modules path because GitHub runner seems to overwite it
     $env:PSModulePath += ":/root/.local/share/powershell/Modules"
