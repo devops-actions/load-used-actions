e1d31c47b9178861d7f3540042556acac64e7a0d
Rob Bos <rajbos@users.noreply.github.com>
2023-09-05 11:53:37 +0000

    Prevent fail inside of Docker container

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM ghcr.io/rajbos/actions-marketplace/powershell:7
 
 COPY /Src/PowerShell/*.ps1 /src/
 
+ENV DOCKER=true
+
 ENTRYPOINT ["pwsh", "/src/entrypoint.ps1"]
--- Src/PowerShell/generic.ps1
+++ Src/PowerShell/generic.ps1
@@ -1,36 +1,42 @@
 $moduleName = "powershell-yaml"
 
-# install a yaml parsing module (already done in the container image)
-if($env:computername -ne "ROB-XPS9700") {
-    Write-Host "PSHOME: [$pshome]" 
+if ($null -eq $env:DOCKER -Or $false -eq $env:DOCKER -Or $env:DOCKER -eq "false") {
+    Write-Host "Not running in a container, check to see if we need to install [$moduleName]"
+    # install a yaml parsing module (already done in the container image)
+    if($env:computername -ne "ROB-XPS9700") {
+        Write-Host "PSHOME: [$pshome]" 
 
-    # check if module is installed locally:
-    $module = Get-Module -name $moduleName
-    if ($null -eq $module) {
-        Write-Host "Module [$moduleName] not found, installing it"
-        Install-Module -Name $moduleName -Force -Scope CurrentUser -AllowClobber
-    }
-    else {
-        Write-Host "Module [$moduleName] found, skipping installation"
-    }
+        # check if module is installed locally:
+        $module = Get-Module -name $moduleName
+        if ($null -eq $module) {
+            Write-Host "Module [$moduleName] not found, installing it"
+            Install-Module -Name $moduleName -Force -Scope CurrentUser -AllowClobber
+        }
+        else {
+            Write-Host "Module [$moduleName] found, skipping installation"
+        }
 
-    # add back the root folder to the modules path because GitHub runner seems to overwite it
-    # first check if module path already has this value:
-    if ($false -eq ($env:PSModulePath -like "/root/.local/share/powershell/Modules")) {
-        $env:PSModulePath += ":/root/.local/share/powershell/Modules"
-    }
+        # add back the root folder to the modules path because GitHub runner seems to overwite it
+        # first check if module path already has this value:
+        if ($false -eq ($env:PSModulePath -like "/root/.local/share/powershell/Modules")) {
+            $env:PSModulePath += ":/root/.local/share/powershell/Modules"
+        }
 
-    Write-Host "PSModulePath:"
-    foreach ($path in $env:PSModulePath -split ':') {
-        Write-Host "- [$path]"
+        Write-Host "PSModulePath:"
+        foreach ($path in $env:PSModulePath -split ':') {
+            Write-Host "- [$path]"
+        }
+        
+        try {
+            Write-Host "Importing module for the yaml parsing"
+            Import-Module powershell-yaml -Force
+        }
+        catch {
+            Write-Warning "Error during importing of the yaml module needed for parsing"
+            Write-Warning $_
+        }
     }
-    
-    try {
-        Write-Host "Importing module for the yaml parsing"
-        Import-Module powershell-yaml -Force
-    }
-    catch {
-        Write-Warning "Error during importing of the yaml module needed for parsing"
-        Write-Warning $_
-    }
+}
+else {
+    Write-Host "Running in a container, [$moduleName] already installed in the base image"
 }
