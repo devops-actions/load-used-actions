8bada8976c34c3f8814402fe18c847b7a9674ea8
Rob Bos <rajbos@users.noreply.github.com>
2023-09-06 20:08:22 +0000

    cleanup

--- Src/PowerShell/generic.ps1
+++ Src/PowerShell/generic.ps1
@@ -1,39 +1,5 @@
 $moduleName = "powershell-yaml"
 
-if ($null -eq $env:DOCKER -Or $false -eq $env:DOCKER -Or $env:DOCKER -eq "false") {
-    Write-Host "Not running in a container, check to see if we need to install [$moduleName]"
-    # install a yaml parsing module (already done in the container image)
-    if($env:computername -ne "ROB-XPS9700") {
-        Write-Host "PSHOME: [$pshome]" 
-        
-        Write-Host "PSModulePaths:"
-        foreach ($path in $env:PSModulePath -split ':') {
-            Write-Host "- [$path]"
-        }
-
-        # check if module is installed locally:
-        $module = Get-Module -name $moduleName
-        if ($null -eq $module) {
-            Write-Host "Module [$moduleName] not found, installing it"
-            #Install-Module -Name $moduleName -Force -Scope CurrentUser -AllowClobber
-        }
-        else {
-            Write-Host "Module [$moduleName] found, skipping installation"
-        }
-    }
-}
-else {
-    Write-Host "Running in a container, [$moduleName] already installed in the base image"
-    # double check then!
-    $module = Get-Module -name $moduleName
-    if ($null -eq $module) {
-        Write-Host "Module [$moduleName] not found!"
-    }
-    else {
-        Write-Host "Module [$moduleName] found"
-    }
-}
-
 # add back the root folder to the modules path because GitHub runner seems to overwite it
 # first check if module path already has this value:
 if ($false -eq ($env:PSModulePath -like "/root/.local/share/powershell/Modules")) {
@@ -45,6 +11,16 @@ try {
     Import-Module powershell-yaml -Force
 }
 catch {
-    Write-Warning "Error during importing of the yaml module needed for parsing"
+    Write-Warning "Error during importing of the yaml module needed for parsing:"
     Write-Warning $_
+
+    Write-Host "Trying to install the module, internet connection needed:"
+    try {
+        Install-Module -Name $moduleName -Force -Scope CurrentUser -AllowClobber
+    }
+    catch {
+        Write-Warning "Error during installation of the yaml module needed for parsing:"
+        Write-Warning $_
+        return
+    }
 }
